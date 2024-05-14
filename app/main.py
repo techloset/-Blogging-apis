a48dc6b77582dd27b4749068e63ac78cb9577db2
zohaib521321 <khanzohaib3231@gmail.com>
2024-05-13 23:28:23 -0700

    Custom exception handler

--- app/main.py
+++ app/main.py
@@ -1,7 +1,25 @@
 from fastapi import FastAPI
 from app.route import auth
 from app.route import post
-app=FastAPI()
-app.include_router(auth.route, prefix="/api/auth",tags=["Authentication"])
-app.include_router(post.route, prefix="/api/post",tags=["Posts"])
+from starlette.responses import JSONResponse
 
+app = FastAPI(
+    title="My FastAPI Application",
+    description="This is a sample FastAPI application with Bloggin app",
+    version="1.0.0"
+)
+@app.exception_handler(404)
+async def not_found(request, exc):
+    return JSONResponse(status_code=404, content={"message": "Route Not found"})
+app.include_router(
+    auth.route,
+    prefix="/api/auth",
+    tags=["Authentication"],
+
+)
+
+app.include_router(
+    post.route,
+    prefix="/api/post",
+    tags=["Posts"],
+)
